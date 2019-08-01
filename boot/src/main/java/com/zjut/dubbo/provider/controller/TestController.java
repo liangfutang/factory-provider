df773201e4d19913837c5643914fc8f11223781b
liangfutang <liangfu.tang@ecarx.com.cn>
2019-08-01 15:07:14 +0800

    修改日志traceID的过滤器

--- boot/src/main/java/com/zjut/dubbo/provider/controller/TestController.java
+++ boot/src/main/java/com/zjut/dubbo/provider/controller/TestController.java
@@ -2,6 +2,8 @@ package com.zjut.dubbo.provider.controller;
 
 import com.zjut.dubbo.provider.response.RestResponse;
 import com.zjut.dubbo.provider.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     private TestService testService;
 
     @RequestMapping("/test")
     public RestResponse test() {
+        logger.info("日志测试");
         System.out.println("进入到测试方法");
         return new RestResponse(testService.getTest());
     }
--- config/src/main/java/com/zjut/dubbo/provider/config/filters/FilterConfig.java
+++ config/src/main/java/com/zjut/dubbo/provider/config/filters/FilterConfig.java
@@ -1,21 +1,25 @@
 package com.zjut.dubbo.provider.config.filters;
 
+import com.zjut.dubbo.provider.config.filters.TraceIdFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig {
 
-    @Autowired
-    private TraceIdFilter traceIdFilter;
+//    @Autowired
+//    private TraceIdFilter traceIdFilter;
 
+    @Bean
     public FilterRegistrationBean setTraceIdFilter() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(traceIdFilter);
+        registration.setFilter(new TraceIdFilter());
         registration.addUrlPatterns("/*");
         registration.setName("traceIdFilter");
         registration.setOrder(1);
         return registration;
     }
+
 }
