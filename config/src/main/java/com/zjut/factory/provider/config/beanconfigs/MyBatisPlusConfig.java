bd464b4c249b43b1d8b58d180deae88676549212
liangfutang <liangfu.tang@ecarx.com.cn>
2020-01-10 10:20:58 +0800

    去除项目中对mybatis-plus和mysql的引用

--- config/src/main/java/com/zjut/factory/provider/config/beanconfigs/MyBatisPlusConfig.java
+++ config/src/main/java/com/zjut/factory/provider/config/beanconfigs/MyBatisPlusConfig.java
@@ -1,18 +1,18 @@
-package com.zjut.factory.provider.config.beanconfigs;
-
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.zjut.dubbo.provider.mapper")
-public class MyBatisPlusConfig {
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor page = new PaginationInterceptor();
-        //设置方言类型
-        page.setDialectType("mysql");
-        return page;
-    }
-}
+//package com.zjut.factory.provider.config.beanconfigs;
+//
+//import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@MapperScan("com.zjut.dubbo.provider.mapper")
+//public class MyBatisPlusConfig {
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        PaginationInterceptor page = new PaginationInterceptor();
+//        //设置方言类型
+//        page.setDialectType("mysql");
+//        return page;
+//    }
+//}
