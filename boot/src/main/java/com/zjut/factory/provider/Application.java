9af9967ee9b54845931a97a945abeda726143653
liangfutang <liangfu.tang@ecarx.com.cn>
2020-03-18 09:42:53 +0800

    去除原先的application引用dubbo的方式，换成starter的模块引用方式

--- boot/src/main/java/com/zjut/factory/provider/Application.java
+++ boot/src/main/java/com/zjut/factory/provider/Application.java
@@ -1,9 +1,13 @@
 package com.zjut.factory.provider;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// TODO 后面想想办法能不能把DubboComponentScan也整合到starter的模块中去
 @SpringBootApplication
+@DubboComponentScan(basePackages = {"com.zjut.factory.provider.openservice.service.impl"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
