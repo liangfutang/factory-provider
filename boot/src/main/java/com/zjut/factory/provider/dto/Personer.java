f9afd3e530cb8d1ff42821f7af51e1ec290cb973
liangfutang <liangfu.tang@ecarx.com.cn>
2020-03-25 13:45:52 +0800

    添加通过builder模式构建实体

--- boot/src/main/java/com/zjut/factory/provider/dto/Personer.java
+++ boot/src/main/java/com/zjut/factory/provider/dto/Personer.java
@@ -12,4 +12,37 @@ public class Personer implements Serializable {
     private String city;
 
     private Integer age;
+
+    public Personer(){}
+
+    private Personer(Builder builder) {
+        name = builder.name;
+        city = builder.city;
+        age = builder.age;
+    }
+
+    public static class Builder {
+        private String name;
+        private String city;
+        private Integer age;
+
+        public Builder name(String var) {
+            name = var;
+            return this;
+        }
+
+        public Builder city(String var) {
+            city = var;
+            return this;
+        }
+
+        public Builder age(Integer var) {
+            age = var;
+            return this;
+        }
+
+        public Personer builder() {
+            return new Personer(this);
+        }
+    }
 }
