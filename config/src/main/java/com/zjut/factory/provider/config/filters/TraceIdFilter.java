c34e3942b2a663da8e66cb6dfdfc29aaf29720d5
liangfutang <liangfu.tang@ecarx.com.cn>
2020-09-11 18:31:02 +0800

    整理maven依赖

--- config/src/main/java/com/zjut/factory/provider/config/filters/TraceIdFilter.java
+++ config/src/main/java/com/zjut/factory/provider/config/filters/TraceIdFilter.java
@@ -4,14 +4,25 @@ import com.zjut.factory.provider.common.constants.Constants;
 import org.slf4j.MDC;
 
 import javax.servlet.*;
+import javax.servlet.FilterConfig;
 import java.io.IOException;
 import java.util.UUID;
 
 public class TraceIdFilter implements Filter {
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         MDC.put(Constants.LOG_TRACE_ID, UUID.randomUUID().toString());
         filterChain.doFilter(servletRequest, servletResponse);
         MDC.clear();
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
